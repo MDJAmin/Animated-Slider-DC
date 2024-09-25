9e07f9fd34f68baf24db936a268eebad16fc0c66
M_Αmin Jafarnejad <mohammadamindf@gmail.com>
2024-09-25 11:12:41 +0330

    Refactor : Add necessary functions

--- js/script.js
+++ js/script.js
@@ -7,3 +7,45 @@ const pauseIcon = document.querySelector("#pause-music")
 
 letPlaying= false;
 
+function togglePlayPause() {
+    console.log("Toggle function called");
+    if (isPlaying) {
+      audio.pause();
+      playIcon.classList.remove("hidden");
+      pauseIcon.classList.add("hidden");
+    } else {
+      audio.play();
+      playIcon.classList.add("hidden");
+      pauseIcon.classList.remove("hidden");
+    }
+    isPlaying = !isPlaying;
+  }
+  
+  playPauseButton.addEventListener("click", togglePlayPause);
+
+audio.addEventListener("ended", function () {
+  audio.currentTime = 0;
+  audio.play();
+});
+
+var swiper = new Swiper(".swiper", {
+    grabCursor: true,
+    initialSlide: 2,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    speed: 1000,
+    freeMode: false,
+    mousewheel: {
+      thresholdDelta: 30,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    on: {
+      click(event) {
+        swiper.slideTo(this.clickedIndex);
+      },
+    },
+  });
+  
