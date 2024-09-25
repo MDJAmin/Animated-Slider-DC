bdfaafc97a90483ad55b4d46e8de4a8f2e38cb86
M_Αmin Jafarnejad <mohammadamindf@gmail.com>
2024-09-25 11:11:36 +0330

    Refactor : get elements from HTML

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 "use strict"
+
+const audio = document.querySelector("#background-music")
+const playPauseButton = document.querySelector("#play-pause-button")
+const playIcon = document.querySelector("#play-music")
+const pauseIcon = document.querySelector("#pause-music")
+
+letPlaying= false;
+
